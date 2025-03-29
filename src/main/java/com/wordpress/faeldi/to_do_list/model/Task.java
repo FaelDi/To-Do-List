1a3c245ac9cb62c88420d290f2a7259f25ad4304
Rafael Dias Ferreira <faeldii@icloud.com>
2025-03-29 02:18:08 -0300

    Task model

--- src/main/java/com/wordpress/faeldi/to_do_list/model/Task.java
+++ src/main/java/com/wordpress/faeldi/to_do_list/model/Task.java
@@ -1,4 +1,25 @@
 package com.wordpress.faeldi.to_do_list.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
