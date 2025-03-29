f4b25c58efa5dd0f2ed7484721eba67dd90696a2
Rafael Dias Ferreira <faeldii@icloud.com>
2025-03-29 02:18:38 -0300

    Adding TaskService and modifing Base service

--- src/main/java/com/wordpress/faeldi/to_do_list/service/BaseService.java
+++ src/main/java/com/wordpress/faeldi/to_do_list/service/BaseService.java
@@ -2,16 +2,16 @@ package com.wordpress.faeldi.to_do_list.service;
 
 import java.util.List;
 
-public interface BaseService<T> {
+public interface BaseService<T,ValueType> {
 
     List<T> findAll();
 
-    T findById(String id);
+    T findById(ValueType id);
 
     T create(T obj);
 
-    T edit(String id, T obj);
+    T edit(ValueType id, T obj);
 
-    void delete(String id);
+    void delete(ValueType id);
 
 }
--- src/main/java/com/wordpress/faeldi/to_do_list/service/TaskService.java
+++ src/main/java/com/wordpress/faeldi/to_do_list/service/TaskService.java
@@ -1,4 +1,43 @@
 package com.wordpress.faeldi.to_do_list.service;
 
-public class TaskService {
+import com.wordpress.faeldi.to_do_list.model.Task;
+import com.wordpress.faeldi.to_do_list.repository.TaskRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TaskService implements BaseService<Task,Long>{
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
+    @Override
+    public List<Task> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Task findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task create(Task obj) {
+        return null;
+    }
+
+    @Override
+    public Task edit(Long id, Task obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
